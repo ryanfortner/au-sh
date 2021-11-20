1f0e48881f955f09e1d8d945e8beb5292ef23b5b
Ryan Fortner <71036629+ryanfortner@users.noreply.github.com>
2021-11-20 12:11:34 -0500

    initial commit

---
+++ au.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+# define variables
+PKGDIR="/root/raspbian-addons/debian/pool/"
+PKGDIRA="/root/raspbian-addons/debian/"
+GPGPATH="/root/gpgpass"
+EMAILPATH="/root/email"
+DATEA="$(date)"
+
+echo $DATEA
+
+function error {
+  echo -e "\e[91m$1\e[39m"
+  exit 1
+}
+
+if [ ! -f ${GPGPASS} ]; then
+  error "gpg file not detected"
+fi
+
+if [ ! -f ${EMAIL} ]; then
+  error "email file not detected"
+fi
+
+mkdir -p ~/dlfiles
+cd ~/dlfiles
+rm -rf *
+
+echo "Updating VSCodium"
+LATEST=`curl -s https://api.github.com/repos/VSCodium/VSCodium/releases/latest | grep -oP '"tag_name": "\K(.*)(?=")'`
+curl -s https://api.github.com/repos/VSCodium/VSCodium/releases/latest \
+  | grep browser_download_url \
+  | grep 'armhf.deb"' \
+  | cut -d '"' -f 4 \
+  | xargs -n 1 curl -L -o codium-$LATEST-armhf.deb || error "Failed to download the codium:armhf"
+
+curl -s https://api.github.com/repos/VSCodium/VSCodium/releases/latest \
+  | grep browser_download_url \
+  | grep 'arm64.deb"' \
+  | cut -d '"' -f 4 \
+  | xargs -n 1 curl -L -o codium-$LATEST-arm64.deb || error "Failed to download codium:arm64"
+
+rm $PKGDIR/codium-* || rm $PKGDIR/codium_*
+
+mv *.deb $PKGDIR
+
+cd $PKGDIRA
+echo "writing packages..."
+EMAIL="$(cat /root/email)"
+GPGPASS="$(cat /root/gpgpass)"
+rm InRelease Release Release.gpg Packages Packages.gz
+dpkg-scanpackages --multiversion . > Packages
+gzip -k -f Packages
+apt-ftparchive release . > Release
+gpg --default-key "${EMAIL}" --batch --pinentry-mode="loopback" --passphrase="$GPGPASS" -abs -o - Release > Release.gpg
+gpg --default-key "${EMAIL}" --batch --pinentry-mode="loopback" --passphrase="$GPGPASS" --clearsign -o - Release > InRelease
+echo "Repository successfully updated."
