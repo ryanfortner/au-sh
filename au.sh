18229e64fcb568a7d9eb3627c7b72cc5c71a4d0e
Ryan Fortner <71036629+ryanfortner@users.noreply.github.com>
2021-11-24 13:40:08 -0500

    Update au.sh

--- au.sh
+++ au.sh
@@ -46,7 +46,7 @@ echo "Updating Goreleaser"
 LATEST=`curl -s https://api.github.com/repos/goreleaser/goreleaser/releases/latest | grep -oP '"tag_name": "\K(.*)(?=")'`
 curl -s https://api.github.com/repos/goreleaser/goreleaser/releases/latest \
   | grep browser_download_url \
-  | grep 'armv6.deb"' \
+  | grep 'armhf.deb"' \
   | cut -d '"' -f 4 \
   | xargs -n 1 curl -L -o goreleaser-$LATEST-armhf.deb || error "Failed to download goreleaser:armhf"
 
