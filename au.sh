e6fac80e071bac874daaa2c8cbd187abaf935f4c
Ryan Fortner <71036629+ryanfortner@users.noreply.github.com>
2021-11-28 09:52:44 -0500

    add hyperfine

--- au.sh
+++ au.sh
@@ -58,6 +58,22 @@ curl -s https://api.github.com/repos/goreleaser/goreleaser/releases/latest \
 
 rm $PKGDIR/goreleaser-* || rm $PKGDIR/goreleaser_*
 
+echo "Updating hyperfine"
+LATEST=`curl -s https://api.github.com/repos/sharkdp/hyperfine/releases/latest | grep -oP '"tag_name": "\K(.*)(?=")'`
+curl -s https://api.github.com/repos/sharkdp/hyperfine/releases/latest \
+  | grep browser_download_url \
+  | grep 'armhf.deb"' \
+  | cut -d '"' -f 4 \
+  | xargs -n 1 curl -L -o hyperfine-$LATEST-armhf.deb || error "Failed to download hyperfine:armhf"
+
+curl -s https://api.github.com/repos/sharkdp/hyperfine/releases/latest \
+  | grep browser_download_url \
+  | grep 'arm64.deb"' \
+  | cut -d '"' -f 4 \
+  | xargs -n 1 curl -L -o hyperfine-$LATEST-arm64.deb || error "Failed to download hyperfine:arm64"
+
+rm $PKGDIR/hyperfine-* || rm $PKGDIR/hyperfine_*
+
 mv *.deb $PKGDIR
 
 cd $PKGDIRA
